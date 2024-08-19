89e622c7d6c1c925cb35779ec7a6892fca9800cd
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-08-19 14:06:44 -0300

    Adding power up super jump

---
+++ Teste3D/Assets/Scripts/Collectables/PowerUpHigherJump.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpHigherJump : ItemCollectable
+{
+    private float increaseHight = 2f;
+    private float timePowerUp = 5f;
+    public override void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            StartCoroutine(BoostJumpHight(col));
+        }
+        
+    }
+
+    IEnumerator BoostJumpHight(Collider col)
+    {
+        col.gameObject.GetComponent<PlayerController>().jumpHeight += increaseHight;
+        yield return new WaitForSeconds(timePowerUp);
+        col.gameObject.GetComponent<PlayerController>().jumpHeight -= increaseHight;
+    }
+}
