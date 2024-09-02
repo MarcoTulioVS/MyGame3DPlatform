8469982d7251ca13e39fca047a56ffc574292f8c
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-09-02 17:33:31 -0300

    Importing Nav mesh components

--- Teste3D/Assets/Scripts/Enemies/Skeleton.cs
+++ Teste3D/Assets/Scripts/Enemies/Skeleton.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Skeleton : MonoBehaviour,IEnemy
 {
     public Enemy enemy;
     private Animator animator;
-    
+    private NavMeshAgent agent;
+
+    public Transform target;
     private void Start()
     {
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     public void Attack()
@@ -19,6 +22,14 @@ public class Skeleton : MonoBehaviour,IEnemy
 
     public void Move()
     {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
 
+    private void Update()
+    {
+        Move();
     }
 }
