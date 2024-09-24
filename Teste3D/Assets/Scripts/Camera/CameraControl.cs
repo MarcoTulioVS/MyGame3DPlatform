5c98cbc3a0b224cc51770429491839e3f9c6b4b2
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-09-24 16:36:26 -0300

    Updating camera movement

--- Teste3D/Assets/Scripts/Camera/CameraControl.cs
+++ Teste3D/Assets/Scripts/Camera/CameraControl.cs
@@ -3,45 +3,38 @@ using UnityEngine.InputSystem;
 
 public class CameraControl : MonoBehaviour
 {
-    public float speed = 10.0f;                // Velocidade de movimento da câmera
-    public float mouseSensitivity = 100.0f;   // Sensibilidade do mouse para rotação
-    public float verticalRotationLimit = 80.0f; // Limite de rotação vertical
+    public CharacterController characterController;
+    public float speed;
+    public float turnSpeed = 1;
+    public float gravity = -9.8f;
 
-    private float pitch = 0.0f; // Rotação vertical
-    private float yaw = 0.0f;   // Rotação horizontal
-    private PlayerInput playerInput; // Referência ao componente PlayerInput
-    private InputAction moveAction; // Ação de movimento
-    private InputAction lookAction; // Ação de olhar (mouse)
+    private float vSpeed;
 
-    private void Awake()
-    {
-        playerInput = GetComponent<PlayerInput>();
-        if (playerInput == null)
-        {
-            Debug.LogError("PlayerInput component is missing from the camera.");
-            return;
-        }
-
-        moveAction = playerInput.actions["Move"];
-        lookAction = playerInput.actions["Look"];
-    }
+    public float jumpSpeed;
+    public Animator animator;
 
     private void Update()
     {
-        // Movimento da câmera
-        Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
-        transform.position += move * speed * Time.deltaTime;
+        transform.Rotate(0,Input.GetAxis("Horizontal")*turnSpeed*Time.deltaTime,0);
+        var inputAxisVertical = Input.GetAxis("Vertical");
+        var speedVector = transform.forward * inputAxisVertical * speed;
 
-        // Rotação da câmera
-        Vector2 lookInput = lookAction.ReadValue<Vector2>();
-        float mouseX = lookInput.x;
-        float mouseY = lookInput.y;
+        if (characterController.isGrounded)
+        {
+            vSpeed = 0;
 
-        yaw += mouseX * mouseSensitivity * Time.deltaTime;
-        pitch -= mouseY * mouseSensitivity * Time.deltaTime;
-        pitch = Mathf.Clamp(pitch, -verticalRotationLimit, verticalRotationLimit);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                vSpeed = jumpSpeed;
+            }
+        }
 
-        transform.localRotation = Quaternion.Euler(pitch, yaw, 0.0f);
+        vSpeed-=gravity*Time.deltaTime;
+        speedVector.y = vSpeed;
+
+        characterController.Move(speedVector * Time.deltaTime);
+        animator.SetBool("Run",inputAxisVertical!=0);
+        
+        
     }
 }
